40988682edc24de7d5b30a6e193bf18a9d6e5a1f
Hung <nguyendinhhung17042005@gmail.com>
2026-04-10 18:14:28 +0700

    Tao thu muc Prefabs va scripts, hoan thanh logic map random

---
+++ Assets/scripts/GameManager.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    // Kéo thả Prefab que diêm vào đây ở bảng Inspector
+    public GameObject matchPrefab;
+    // Kéo thả 5 cái Row (hàng) vào danh sách này
+    public Transform[] rows;
+
+    void Start()
+    {
+        GenerateMap();
+    }
+
+    void GenerateMap()
+    {
+        foreach (Transform row in rows)
+        {
+            // Mỗi hàng sẽ có từ 1 đến 10 que ngẫu nhiên
+            int count = Random.Range(1, 11);
+            for (int i = 0; i < count; i++)
+            {
+                Instantiate(matchPrefab, row);
+            }
+        }
+    }
+}
